2ddffcbf451dca6bf224929f06de0b20903f35f1
Moustafa Mohamed <Moustafa Mohamed@192.168.1.32>
2020-07-14 17:21:12 +0200

    Merge origin/master
    Conflicts:
    	src/b4934/systemutils/Obstacle.java
    	src/b4934/testing/TestFrame.java

---
+++ src/b4934/systemutils/core/Constants.java
@@ -0,0 +1,6 @@
+package b4934.systemutils.core;
+
+public class Constants {
+
+    public static final int LEVEL_HEIGHT = 800;
+}
---
+++ src/b4934/testing/TestFrame.java
@@ -0,0 +1,54 @@
+package b4934.testing;
+
+import b4934.systemutils.Player;
+import b4934.systemutils.core.GameObject;
+import b4934.systemutils.core.Level;
+import java.awt.Dimension;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class TestFrame extends JFrame {
+
+    JButton play;
+    JButton stop;
+    Player player;
+    Level level1;
+
+    public TestFrame() {
+        setSize(800, 600);
+        setVisible(true);
+        setResizable(false);
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        initComponents();
+    }
+
+    private void initComponents() {
+        play = new JButton("Play");
+        play.setLocation(400, 50);
+        play.setSize(100, 100);
+        play.setPreferredSize(new Dimension(100, 100));
+        play.addActionListener((al) -> {
+            player.setIsMoving(true);
+        });
+        add(play);
+
+        stop = new JButton("Stop");
+        stop.setLocation(400, 200);
+        stop.setSize(60, 60);
+        stop.setPreferredSize(new Dimension(100, 100));
+        stop.addActionListener((al) -> {
+            player.setIsMoving(false);
+        });
+        add(stop);
+
+        player = new Player(70, 70,"", 10);
+        player.setLocation(50, 50);
+        add(player);
+        
+        level1 = new Level("level1", 1311);
+        add(level1);
+        repaint();
+    }
+
+}
